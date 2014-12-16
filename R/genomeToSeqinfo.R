9d8a032b2109d773f86200930283f2c734bf173e
Herve Pages <hpages@fhcrc.org>
2014-12-16 00:33:16 +0000

    clarify need for internet access when calling 'Seqinfo(genome="some genome")'
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomeInfoDb@97676 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/genomeToSeqinfo.R
+++ R/genomeToSeqinfo.R
@@ -16,11 +16,25 @@
         circ_idx <- match(circ_seqs, ans_seqnames)
         if (any(is.na(circ_idx)))
             stop("'circ_seqs' contains sequence names not in ",
-                 genome, " genome")
+                 "\"", genome, "\" genome")
         ans_isCircular[circ_idx] <- TRUE
     }
     Seqinfo(ans_seqnames, ans_seqlengths, ans_isCircular, genome)
 }
+
+#.NCBI_genome_to_Seqinfo <- function(refseq_assembly_id, AssemblyUnits,
+#                                                        circ_seqs)
+#{
+#    assembly_report <- fetch_assembly_report(refseq_assembly_id,
+#                                             AssemblyUnits=AssemblyUnits)
+#    ans_seqnames <- as.character(assembly_report[ , "SequenceName"])
+#
+#}
+#
+#SUPPORTED_NCBI_GENOMES <- list(
+#    GRCh38=
+#        list(refseq_assembly_id="GCF_000001405.26", circular="MT")
+#)
 
 ### Only supports UCSC genomes for now (the same genomes that are supported
 ### by fetchExtendedChromInfoFromUCSC()).
@@ -30,9 +44,20 @@ genomeToSeqinfo <- function(genome)
     if (!isSingleString(genome) || genome == "")
         stop("'genome' must be a single non-empty string")
     idx <- match(genome, names(SUPPORTED_UCSC_GENOMES))
-    if (is.na(idx))
-        stop("genome \"", genome, "\" is not supported")
-    circ_seqs <- SUPPORTED_UCSC_GENOMES[[idx]]$circular
-    .UCSC_genome_to_Seqinfo(genome, circ_seqs)
+    if (!is.na(idx)) {
+        supported_genome <- SUPPORTED_UCSC_GENOMES[[idx]]
+        circ_seqs <- supported_genome$circular
+        return(.UCSC_genome_to_Seqinfo(genome, circ_seqs))
+    }
+    #idx <- match(genome, names(SUPPORTED_NCBI_GENOMES))
+    #if (!is.na(idx)) {
+    #    supported_genome <- SUPPORTED_NCBI_GENOMES[[idx]]
+    #    refseq_assembly_id <- supported_genome$refseq_assembly_id
+    #    AssemblyUnits <- supported_genome$AssemblyUnits
+    #    circ_seqs <- supported_genome$circular
+    #    return(.NCBI_genome_to_Seqinfo(refseq_assembly_id, AssemblyUnits,
+    #                                                       circ_seqs,))
+    #}
+    stop("genome \"", genome, "\" is not supported")
 }
 
