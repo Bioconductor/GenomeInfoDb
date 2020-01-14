b654fee065b336eb8bc1dc30f928304a16b9bb1e
Hervé Pagès <hpages@fredhutch.org>
2020-01-13 17:45:59 -0800

    Register a few more UCSC genome assemblies

---
+++ inst/registered_genomes/UCSC/bosTau3.R
@@ -0,0 +1,51 @@
+### Should work as a standlone, self-contained script.
+### Must define at least:
+###   o GENOME:              Single non-empty string.
+###   o ORGANISM:            Single non-empty string.
+###   o ASSEMBLED_MOLECULES: Character vector with no NAs, no empty strings,
+###                          and no duplicates.
+### Can also define:
+###   o CIRC_SEQS:           Character vector (subset of ASSEMBLED_MOLECULES).
+###   o GET_CHROM_SIZES:     Function with 1 argument. Must return a 2-column
+###                          data.frame with columns "chrom" and "size".
+GENOME <- "bosTau3"
+ORGANISM <- "Bos taurus"
+ASSEMBLED_MOLECULES <- paste0("chr", c(1:29, "X"))
+
+CIRC_SEQS <- character(0)
+
+library(IRanges)       # for CharacterList()
+library(GenomeInfoDb)  # for fetch_chrom_sizes_from_UCSC()
+
+.order_seqlevels <- function(seqlevels)
+{
+    tmp <- CharacterList(strsplit(seqlevels, ".", fixed=TRUE))
+    npart <- lengths(tmp)
+    stopifnot(all(npart %in% c(1L, 3L)))
+
+    idx1 <- which(npart == 1L)
+    stopifnot(length(idx1) == length(ASSEMBLED_MOLECULES))
+    oo1 <- match(ASSEMBLED_MOLECULES, seqlevels[idx1])
+    stopifnot(!anyNA(oo1))
+    idx1 <- idx1[oo1]
+
+    idx3 <- which(npart == 3L)
+    m3 <- matrix(unlist(tmp[idx3]), ncol=3L, byrow=TRUE)
+    stopifnot(all(m3[ , 1L] == "chrUn"))
+    stopifnot(all(m3[ , 2L] == "003"))
+    m33 <- as.integer(m3[ , 3L])
+    stopifnot(!anyNA(m33))
+    oo3 <- order(m33)
+    idx3 <- idx3[oo3]
+
+    c(idx1, idx3)
+}
+
+GET_CHROM_SIZES <- function(goldenPath.url=getOption("UCSC.goldenPath.url"))
+{
+    chrom_sizes <- GenomeInfoDb:::fetch_chrom_sizes_from_UCSC(GENOME,
+                                              goldenPath.url=goldenPath.url)
+    oo <- .order_seqlevels(chrom_sizes[ , "chrom"])
+    S4Vectors:::extract_data_frame_rows(chrom_sizes, oo)
+}
+
