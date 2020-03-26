0b16aeb06b195a5cee6814598301c702f00d7d4c
Hervé Pagès <hpages@fredhutch.org>
2020-03-26 09:43:00 -0700

    Register NCBI genome dvir_caf1 (Drosophila virilis)

---
+++ inst/registered/NCBI_assemblies/Drosophila_virilis.R
@@ -0,0 +1,11 @@
+ORGANISM <- "Drosophila virilis"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    list(assembly="dvir_caf1",
+         date="2006/07/12",
+         extra_info=c(strain="TSC#15010-1051.87"),
+         assembly_accession="GCA_000005245.1",  # droVir3
+         circ_seqs=character(0))
+)
+
