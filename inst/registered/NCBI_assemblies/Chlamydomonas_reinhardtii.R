e2405ec4b19d728beedb45816acbdf408ddb5f71
Hervé Pagès <hpages.on.github@gmail.com>
2020-11-16 14:42:08 -0800

    Register NCBI assembly Chlamydomonas_reinhardtii_v5.5

---
+++ inst/registered/NCBI_assemblies/Chlamydomonas_reinhardtii.R
@@ -0,0 +1,10 @@
+ORGANISM <- "Chlamydomonas reinhardtii"
+
+ASSEMBLIES <- list(
+    list(assembly="Chlamydomonas_reinhardtii_v5.5",
+         date="2018/01/26",
+         extra_info=c(strain="CC-503 cw92 mt+"),
+         assembly_accession="GCA_000002595.3",
+         circ_seqs=character(0))
+)
+
