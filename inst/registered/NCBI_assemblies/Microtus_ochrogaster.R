6fb1a298b479a5b25b2812071dd750104c1297d6
Hervé Pagès <hpages.on.github@gmail.com>
2020-12-07 23:42:37 -0800

    Register NCBI assembly MicOch1.0

---
+++ inst/registered/NCBI_assemblies/Microtus_ochrogaster.R
@@ -0,0 +1,10 @@
+ORGANISM <- "Microtus ochrogaster"
+
+ASSEMBLIES <- list(
+    list(assembly="MicOch1.0",
+         date="2012/12/07",
+         extra_info=c(sex="female"),
+         assembly_accession="GCA_000317375.1",
+         circ_seqs=character(0))
+)
+
