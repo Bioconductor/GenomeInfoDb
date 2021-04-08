2eee6d70ab79a3c78d2ab45365045ea8312d6920
Hervé Pagès <hpages.on.github@gmail.com>
2021-04-08 01:38:43 -0700

    Register NCBI assembly Bra_napus_v2.0

---
+++ inst/registered/NCBI_assemblies/Brassica_napus.R
@@ -0,0 +1,10 @@
+ORGANISM <- "Brassica napus"
+  
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    list(assembly="Bra_napus_v2.0",
+         date="2017/09/15",
+         assembly_accession="GCF_000686985.2",
+         circ_seqs=c("MT", "Pltd"))
+)
+
