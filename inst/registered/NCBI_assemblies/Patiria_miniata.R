62a2e8673700411c0a650bf0476142b57764d6a9
Hervé Pagès <hpages.on.github@gmail.com>
2021-05-10 12:03:50 -0700

    Register NCBI assemblies Pmin_1.0 and Pmin_3.0

---
+++ inst/registered/NCBI_assemblies/Patiria_miniata.R
@@ -0,0 +1,15 @@
+ORGANISM <- "Patiria miniata"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    list(assembly="Pmin_1.0",
+         date="2012/08/13",
+         assembly_accession="GCA_000285935.1",
+         circ_seqs=character(0)),
+
+    list(assembly="Pmin_3.0",
+         date="2020/12/01",
+         assembly_accession="GCF_015706575.1",
+         circ_seqs=character(0))
+)
+
