5409f1eab6cf3cb13ea7b9abde1ac5bdc17f0c5e
Hervé Pagès <hpages.on.github@gmail.com>
2021-06-25 02:56:23 -0700

    Register NCBI assembly MU-UCD_Fhet_4.1 (GCA_011125445.2)

---
+++ inst/registered/NCBI_assemblies/Fundulus_heteroclitus.R
@@ -0,0 +1,10 @@
+ORGANISM <- "Fundulus heteroclitus"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    list(assembly="MU-UCD_Fhet_4.1",
+         date="2020/08/13",
+         assembly_accession="GCA_011125445.2",
+         circ_seqs="MT")
+)
+
