41b17e80e012d3f998d6d2db165fa8e287a19b15
Hervé Pagès <hpages.on.github@gmail.com>
2022-01-28 11:19:13 -0800

    Register some soybean assemblies from NCBI

---
+++ inst/registered/NCBI_assemblies/Glycine_max.R
@@ -0,0 +1,37 @@
+ORGANISM <- "Glycine max"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    ## 1192 sequences.
+    list(assembly="Glycine_max_v2.1",
+         assembly_level="Chromosome",
+         date="2018/07/24",
+         extra_info=c(Cultivar="Williams 82"),
+         assembly_accession="GCF_000004515.5",
+         circ_seqs=c("MT", "Pltd")),
+
+    ## 284 sequences.
+    list(assembly="Glycine_max_v4.0",
+         assembly_level="Chromosome",
+         date="2021/03/10",
+         extra_info=c(Cultivar="Williams 82"),
+         assembly_accession="GCF_000004515.6",
+         circ_seqs=c("MT", "Pltd")),
+
+    ## 468 sequences.
+    list(assembly="ASM2049715v1",
+         assembly_level="Chromosome",
+         date="2021/10/14",
+         extra_info=c(Cultivar="Hwangkeum"),
+         assembly_accession="GCA_020497155.1",
+         circ_seqs=character(0)),
+
+    ## 95 sequences.
+    list(assembly="GmBRS537.a1.v2",
+         assembly_level="Chromosome",
+         date="2021/10/15",
+         extra_info=c(Cultivar="EMBRAPA BRS 537"),
+         assembly_accession="GCA_012273815.2",
+         circ_seqs=c("MT", "Pltd"))
+)
+
