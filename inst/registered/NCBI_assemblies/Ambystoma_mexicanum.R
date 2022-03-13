b0384d4e3bcfff0f8332034cc92c0f3e1a5d091b
Hervé Pagès <hpages.on.github@gmail.com>
2022-03-13 14:34:44 -0700

    Register axolotl (Ambystoma mexicanum) assemblies from NCBI

---
+++ inst/registered/NCBI_assemblies/Ambystoma_mexicanum.R
@@ -0,0 +1,37 @@
+ORGANISM <- "Ambystoma mexicanum"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    ## 611505 sequences.
+    list(assembly="AmbMex13_14.1.0",
+         assembly_level="Scaffold",
+         date="2015/11/25",
+         extra_info=c(strain="laboratory"),
+         assembly_accession="GCA_001455525.1",
+         circ_seqs=character(0)),
+
+    ## 125724 sequences.
+    list(assembly="ASM291563v1",
+         assembly_level="Scaffold",
+         date="2018/02/05",
+         extra_info=c(strain="DD151"),
+         assembly_accession="GCA_002915635.1",
+         circ_seqs=character(0)),
+
+    ## 98070 sequences.
+    list(assembly="ASM291563v2",
+         assembly_level="Chromosome",
+         date="2018/12/04",
+         extra_info=c(strain="DD151"),
+         assembly_accession="GCA_002915635.2",
+         circ_seqs=character(0)),
+
+    ## 27157 sequences.
+    list(assembly="AmbMex60DD",
+         assembly_level="Chromosome",
+         date="2021/04/01",
+         extra_info=c(strain="DD151"),
+         assembly_accession="GCA_002915635.3",
+         circ_seqs=character(0))
+)
+
--- inst/registered/NCBI_assemblies/Glycine_max.R
+++ inst/registered/NCBI_assemblies/Glycine_max.R
@@ -6,7 +6,7 @@ ASSEMBLIES <- list(
     list(assembly="Glycine_max_v2.1",
          assembly_level="Chromosome",
          date="2018/07/24",
-         extra_info=c(Cultivar="Williams 82"),
+         extra_info=c(cultivar="Williams 82"),
          assembly_accession="GCF_000004515.5",
          circ_seqs=c("MT", "Pltd")),
 
@@ -14,7 +14,7 @@ ASSEMBLIES <- list(
     list(assembly="Glycine_max_v4.0",
          assembly_level="Chromosome",
          date="2021/03/10",
-         extra_info=c(Cultivar="Williams 82"),
+         extra_info=c(cultivar="Williams 82"),
          assembly_accession="GCF_000004515.6",
          circ_seqs=c("MT", "Pltd")),
 
@@ -22,7 +22,7 @@ ASSEMBLIES <- list(
     list(assembly="ASM2049715v1",
          assembly_level="Chromosome",
          date="2021/10/14",
-         extra_info=c(Cultivar="Hwangkeum"),
+         extra_info=c(cultivar="Hwangkeum"),
          assembly_accession="GCA_020497155.1",
          circ_seqs=character(0)),
 
@@ -30,7 +30,7 @@ ASSEMBLIES <- list(
     list(assembly="GmBRS537.a1.v2",
          assembly_level="Chromosome",
          date="2021/10/15",
-         extra_info=c(Cultivar="EMBRAPA BRS 537"),
+         extra_info=c(cultivar="EMBRAPA BRS 537"),
          assembly_accession="GCA_012273815.2",
          circ_seqs=character(0))
 )
