fe2a658dff61542da17e347e3c3fbef4bef70ec0
Hervé Pagès <hpages.on.github@gmail.com>
2022-08-08 13:53:23 -0700

    Repair unit test broken by recent MonDom5 assembly change
    MonDom5 assembly (https://www.ncbi.nlm.nih.gov/assembly/GCF_000002295.2/)
    used to have only 5016 sequences, but, on 2022/06/27, someone added 4022
    sequences to it, instead of submitting a new version of the assembly like
    everybody else.
    Who would have known that NCBI allows submitters to silently modify an
    assembly that they submitted years before without any mention on the
    assembly landing page, without assigning it a new RefSeq assembly accession,
    and without even bumping the assembly date!

--- inst/registered/NCBI_assemblies/Monodelphis_domestica.R
+++ inst/registered/NCBI_assemblies/Monodelphis_domestica.R
@@ -2,6 +2,15 @@ ORGANISM <- "Monodelphis domestica"
 
 ### List of assemblies by date.
 ASSEMBLIES <- list(
+    ## 9038 sequences. Note that this assembly used to have only 5016
+    ## sequences but, on 2022/06/27, someone added 4022 sequences to it.
+    ## The new sequences are all the sequences after the MT line in
+    ## GCF_000002295.2_MonDom5_assembly_report.txt. Yeah they did this,
+    ## instead of submitting a new version of the assembly like everybody
+    ## else! Also who would have known that NCBI allows submitters to
+    ## silently modify an assembly that they submitted years before without
+    ## any mention on the assembly landing page, without assigning it a new
+    ## RefSeq assembly accession, and without even bumping the assembly date!
     list(assembly="MonDom5",
          date="2007/01/25",
          assembly_accession="GCF_000002295.2",  # monDom5
