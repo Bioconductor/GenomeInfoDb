ebb438e5675c8a48284a4849fc6a461da8502ef8
Kirabo Kakopo <kiraboismyname@gmail.com>
2022-10-21 13:09:43 +0300

    Register NCBI assembly Felis_catus_9.0

---
+++ inst/registered/NCBI_assemblies/Felis_catus.R
@@ -0,0 +1,10 @@
+ORGANISM <- "Felis catus"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    list(assembly="Felis_catus_9.0",
+         date="2017/11/20",
+         extra_info=c(breed="Abyssinian"),
+         assembly_accession="GCF_000181335.3",  # felCat9
+         circ_seqs="MT")
+)
