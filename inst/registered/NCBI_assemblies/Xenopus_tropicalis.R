41dc808afdbdac10cdb9d7dabe38598f57cc87cc
Simplecodez <elenduemmanuelelano@gmail.com>
2022-10-25 19:43:38 +0100

    Register NCBI assembly UCB_Xtro_10.0

--- inst/registered/NCBI_assemblies/Xenopus_tropicalis.R
+++ inst/registered/NCBI_assemblies/Xenopus_tropicalis.R
@@ -4,14 +4,14 @@ ORGANISM <- "Xenopus tropicalis"
 ASSEMBLIES <- list(
     ## strain: Nigerian
     list(assembly="UCB_Xtro_10.0",
-    	 assembly_level="Chromosome",
+         assembly_level="Chromosome",
          date="2019/11/14",
          extra_info=c(strain="Nigerian"),
          assembly_accession="GCF_000004195.4",
          circ_seqs="MT"),
 
     list(assembly="ASM1336827v1",
-    	 assembly_level="Chromosome",
+         assembly_level="Chromosome",
          date="2020/06/23",
          extra_info=c(strain="Nigerian"),
          assembly_accession="GCA_013368275.1",
