721160425aff020f5bd7f76602c9abbb4767f8dd
Kirabo Kakopo <kiraboismyname@gmail.com>
2023-02-28 16:20:58 +0300

    Register NCBI assembly Kamilah_GGO_v0

---
+++ inst/registered/NCBI_assemblies/Gorilla_gorilla_gorilla.R
@@ -0,0 +1,10 @@
+ORGANISM <- "Gorilla gorilla gorilla"
+
+## 5486 sequences.
+ASSEMBLIES <- list(
+    list(assembly="Kamilah_GGO_v0",
+         date="2019/08/28",
+         extra_info=c(breed="western lowland gorilla"),
+         assembly_accession="GCF_008122165.1",  # gorGor6
+         circ_seqs="MT")
+)
