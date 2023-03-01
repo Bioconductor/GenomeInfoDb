a026dfeb10122b8acd322f44c6aa64be50ac3138
Hervé Pagès <hpages.on.github@gmail.com>
2023-03-01 13:09:29 -0800

    small tweaks

--- inst/registered/NCBI_assemblies/Gorilla_gorilla_gorilla.R
+++ inst/registered/NCBI_assemblies/Gorilla_gorilla_gorilla.R
@@ -1,8 +1,9 @@
 ORGANISM <- "Gorilla gorilla gorilla"
 
-## 5486 sequences.
 ASSEMBLIES <- list(
+    ## 5486 sequences.
     list(assembly="Kamilah_GGO_v0",
+         assembly_level="Chromosome",
          date="2019/08/28",
          extra_info=c(breed="western lowland gorilla"),
          assembly_accession="GCF_008122165.1",  # gorGor6
