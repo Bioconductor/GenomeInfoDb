89f208cdbe8fb7f1a3b566737f98897657ba9461
Kirabo Kakopo <52150109+kakopo@users.noreply.github.com>
2023-03-13 20:45:23 +0300

    Link gorGor6 (UCSC genome) to Kamilah_GGO_v0 (NCBI assembly) (#90)

--- inst/registered/UCSC_genomes/gorGor6.R
+++ inst/registered/UCSC_genomes/gorGor6.R
@@ -45,3 +45,8 @@ FETCH_ORDERED_CHROM_SIZES <-
     oo <- .order_seqlevels(chrom_sizes[ , "chrom"])
     S4Vectors:::extract_data_frame_rows(chrom_sizes, oo)
 }
+
+NCBI_LINKER <- list(
+  assembly_accession="GCF_008122165.1",
+  special_mappings=c(chrM="MT")
+)
