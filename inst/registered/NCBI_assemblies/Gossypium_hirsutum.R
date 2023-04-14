f4ceb7fc671ad53446654394244ccbeef3000c6a
Hervé Pagès <hpages.on.github@gmail.com>
2023-04-14 00:29:33 -0700

    Register Gossypium_hirsutum_v2.1 assembly from NCBI

---
+++ inst/registered/NCBI_assemblies/Gossypium_hirsutum.R
@@ -0,0 +1,13 @@
+ORGANISM <- "Gossypium hirsutum"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    ## 1027 sequences.
+    list(assembly="Gossypium_hirsutum_v2.1",
+         assembly_level="Chromosome",
+         date="2021/02/25",
+         ## GCA_007990345.1 and GCF_007990345.1 are identical.
+         assembly_accession="GCF_007990345.1",
+         circ_seqs=c("MT", "Pltd"))
+)
+
