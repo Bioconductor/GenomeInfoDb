b5cc7a255786f9a706788a0abbb2579d8d789c37
Hervé Pagès <hpages.on.github@gmail.com>
2023-08-25 10:18:05 -0700

    Register a few Salmo salar (Atlantic salmon) assemblies from NCBI

---
+++ inst/registered/NCBI_assemblies/Salmo_salar.R
@@ -0,0 +1,34 @@
+ORGANISM <- "Salmo salar"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    ## 4011 sequences.
+    list(assembly="Ssal_v3.1",
+         assembly_level="Chromosome",
+         date="2021/04/21",
+         ## GCA_905237065.2 and GCF_905237065.1 are identical.
+         assembly_accession="GCF_905237065.1",
+         circ_seqs=character(0)),
+
+    ## 1149 sequences.
+    list(assembly="USDA_NASsal_1.1",
+         assembly_level="Chromosome",
+         date="2022/01/12",
+         assembly_accession="GCA_021399835.1",
+         circ_seqs=character(0)),
+
+    ## 2653 sequences.
+    list(assembly="Ssal_Brian_v1.0",
+         assembly_level="Chromosome",
+         date="2022/04/01",
+         assembly_accession="GCA_923944775.1",
+         circ_seqs=character(0)),
+
+    ## 3596 sequences.
+    list(assembly="Ssal_ALTA",
+         assembly_level="Chromosome",
+         date="2022/05/11",
+         assembly_accession="GCA_931346935.2",
+         circ_seqs=character(0))
+)
+
