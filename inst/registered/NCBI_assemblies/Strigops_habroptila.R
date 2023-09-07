ed0721b8701f3e049023f6855f49b9b3f56b76b7
Hervé Pagès <hpages.on.github@gmail.com>
2023-09-07 10:03:17 -0700

    Register bStrHab1.2.pri assembly (Kakapo) from NCBI

---
+++ inst/registered/NCBI_assemblies/Strigops_habroptila.R
@@ -0,0 +1,13 @@
+ORGANISM <- "Strigops habroptila"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    ## 90 sequences.
+    list(assembly="bStrHab1.2.pri",
+         assembly_level="Chromosome",
+         date="2020/03/04",
+         ## GCA_004027225.2 and GCF_004027225.2 are identical.
+         assembly_accession="GCF_004027225.2",
+         circ_seqs="bStrHab1_MT")
+)
+
