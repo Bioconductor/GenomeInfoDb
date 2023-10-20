c4bab22a416c96eaeca7ecec2673ea2f50ab9040
Hervé Pagès <hpages.on.github@gmail.com>
2023-10-19 19:18:56 -0700

    Switch from HTTP to HTTPS for requests to *.ucsc.edu

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname)
 {
-    UCSC.goldenPath.url <- "http://hgdownload.cse.ucsc.edu/goldenPath"
+    UCSC.goldenPath.url <- "https://hgdownload.cse.ucsc.edu/goldenPath"
     options(list(UCSC.goldenPath.url=UCSC.goldenPath.url))
 }
 
