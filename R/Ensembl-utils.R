d762e13c9eae03f1cb5e20dbc20c4c3f0de44932
Hervé Pagès <hpages.on.github@gmail.com>
2023-12-13 16:34:29 -0800

    Move internal utility fetch_table_from_Ensembl_FTP() to its own file

--- R/Ensembl-utils.R
+++ R/Ensembl-utils.R
@@ -270,11 +270,11 @@ fetch_species_index_from_Ensembl_FTP <- function(release=NA, division=NA)
               "\n    ", url)
 }
 
-### Find the core db in 'core_dbs' that matches 'species'.
+### Find the core DB in 'core_dbs' that matches 'species'.
 .lookup_species_in_core_dbs <- function(species, core_dbs)
 {
     trimmed_core_dbs <- sub("_core_.*$", "", core_dbs)
-    ## First assume that 'species' was supplied as the name of a core db
+    ## First assume that 'species' was supplied as the name of a core DB
     ## (after removal of the _core_.* suffix) e.g. "homo_sapiens".
     idx <- grep(species, trimmed_core_dbs)
     if (length(idx) == 1L)
@@ -545,130 +545,8 @@ get_Ensembl_FTP_core_db_url <- function(species, release=NA, division=NA,
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### Ensembl Core schema (only for the tables we care about)
+### Fetch core DB data from the Ensembl FTP server
 ###
-### List of Ensembl public MySQL servers / ports
-###   https://www.ensembl.org/info/data/mysql.html
-### Ensembl Core Schema:
-###   https://www.ensembl.org/info/docs/api/core/core_schema.html
-
-### Fundamental Tables
-
-### 'attrib_type' table.
-### (331 rows in homo_sapiens_core_99_38)
-.ENSEMBLDB_ATTRIB_TYPE_COLUMNS <- c(
-    "attrib_type_id",         # primary key
-    "code",
-    "name",
-    "description"
-)
-
-### Assembly Tables
-
-### 'seq_region' table.
-### (268443 rows in homo_sapiens_core_99_38)
-.ENSEMBLDB_SEQ_REGION_COLUMNS <- c(
-    "seq_region_id",          # primary key
-    "name",
-    "coord_system_id",        # ==> coord_system.coord_system_id
-    "length"
-)
-
-### 'coord_system' table.
-### (9 rows in homo_sapiens_core_99_38)
-.ENSEMBLDB_COORD_SYSTEM_COLUMNS <- c(
-    "coord_system_id",        # primary key
-    "species_id",
-    "name",
-    "version",
-    "rank",
-    "attrib"
-)
-
-### 'seq_region_attrib' table.
-### (5927 rows in homo_sapiens_core_99_38)
-.ENSEMBLDB_SEQ_REGION_ATTRIB_COLUMNS <- c(
-    "seq_region_id",          # ==> seq_region.seq_region_id
-    "attrib_type_id",         # ==> attrib_type.attrib_type_id
-    "value"
-)
-
-### 'seq_region_synonym' table.
-### (2360 rows in homo_sapiens_core_99_38)
-.ENSEMBLDB_SEQ_REGION_SYNONYM_COLUMNS <- c(
-    "seq_region_synonym_id",  # primary key
-    "seq_region_id",          # ==> seq_region.seq_region_id
-    "synonym",
-    "external_db_id"          # ==> external_db.external_db_id
-)
-
-### External References
-
-### 'external_db' table.
-### (446 rows in homo_sapiens_core_99_38)
-.ENSEMBLDB_EXTERNAL_DB_COLUMNS <- c(
-    "external_db_id",         # primary key
-    "db_name",
-    "db_release",
-    "status",
-    "priority",
-    "db_display_name",
-    "type",
-    "secondary_db_name",
-    "secondary_db_table",
-    "description"
-)
-
-.ENSEMBLDB_COLUMNS <- list(
-    attrib_type=.ENSEMBLDB_ATTRIB_TYPE_COLUMNS,
-    seq_region=.ENSEMBLDB_SEQ_REGION_COLUMNS,
-    coord_system=.ENSEMBLDB_COORD_SYSTEM_COLUMNS,
-    seq_region_attrib=.ENSEMBLDB_SEQ_REGION_ATTRIB_COLUMNS,
-    seq_region_synonym=.ENSEMBLDB_SEQ_REGION_SYNONYM_COLUMNS,
-    external_db=.ENSEMBLDB_EXTERNAL_DB_COLUMNS
-)
-
-
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### Fetch core db data from the Ensembl FTP server (via RCurl)
-###
-### The utililities in this section use RCurl or just utils::download.file()
-### for getting core db stuff directly from the Ensembl FTP server. They can
-### access stuff that is not available thru biomaRt like for example the
-### lengths of the sequences in the reference genome associated with a
-### particular dataset and Ensembl release (e.g. for dataset
-### "hsapiens_gene_ensembl" in release "64").
-###
-### Note that querying the Ensembl MySQL server (via RMariaDB) would
-### probably be a better way to do this.
-### Update (Feb 10, 2020): Some preliminary testing indicates that using
-### RMariaDB to fetch full tables is actually significantly slower.
-### For example, to fetch table "seq_region" from db "homo_sapiens_core_99_38"
-### (the table has 268443 rows and 4 columns):
-###   o The utils::download.file() + utils::read.table() method takes about
-###     7 sec to download and parse the MySQL dump located at:
-###         ftp://ftp.ensembl.org/pub/current_mysql/homo_sapiens_core_99_38/
-###   o The RMariaDB method takes about 22 sec to retrieve the table from the
-###     MySQL server at ensembldb.ensembl.org.
-###
-
-### Uses the utils::download.file() + utils::read.table() method.
-fetch_table_from_Ensembl_FTP <- function(core_db_url, table,
-                                         full.colnames=FALSE, nrows=-1L)
-{
-    columns <- .ENSEMBLDB_COLUMNS[[table]]
-    if (is.null(columns)) {
-        warning(wmsg("unknown table: ", table, " (download might fail or ",
-                     "the returned data frame will have automatic colnames)"))
-    } else if (full.colnames) {
-        columns <- paste(table, columns, sep=".")
-    }
-    url <- paste0(core_db_url, table, ".txt.gz")
-    ans <- fetch_table_from_url(url, colnames=columns, nrows=nrows)
-    if (full.colnames && is.null(columns))
-        colnames(ans) <- paste(table, colnames(ans), sep=".")
-    ans
-}
 
 fetch_default_coord_systems_from_Ensembl_FTP <- function(core_db_url)
 {
@@ -693,7 +571,7 @@ fetch_default_coord_systems_from_Ensembl_FTP <- function(core_db_url)
 ### Return a list parallel to 'seq_region_ids' where each list element is a
 ### named character vector containing all the synonyms for the corresponding
 ### sequence id. The names on the character vector indicate the origin of
-### the synonym i.e. the external db where it's used (e.g. "INSDC",
+### the synonym i.e. the external DB where it's used (e.g. "INSDC",
 ### "RefSeq_genomic", "UCSC", "ensembl_internal_synonym", etc..)
 .fetch_synonyms_from_Ensembl_FTP <- function(core_db_url, seq_region_ids)
 {
