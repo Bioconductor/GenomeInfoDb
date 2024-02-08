77f3c97cb78607a169b24ab1f827453e04cdd6a0
Hervé Pagès <hpages.on.github@gmail.com>
2024-02-07 17:52:55 -0800

    Use https://hgdownload.soe.ucsc.edu/goldenPath instead of https://hgdownload.cse.ucsc.edu/goldenPath
    The cse address is deprecated. See:
    https://groups.google.com/a/soe.ucsc.edu/g/genome/c/zxS5jah4eZo/m/Jxuprb9BAQAJ

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname)
 {
-    UCSC.goldenPath.url <- "https://hgdownload.cse.ucsc.edu/goldenPath"
+    UCSC.goldenPath.url <- "https://hgdownload.soe.ucsc.edu/goldenPath"
     options(list(UCSC.goldenPath.url=UCSC.goldenPath.url))
 }
 
