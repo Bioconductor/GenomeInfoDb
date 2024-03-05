59eaa9045855fcb40e8ab76c5bc9659ea7656914
Hervé Pagès <hpages.on.github@gmail.com>
2024-03-04 18:56:43 -0800

    Get rid of RCurl

--- R/list_ftp_dir.R
+++ R/list_ftp_dir.R
@@ -10,40 +10,13 @@
     stopifnot(isSingleString(ftp_dir))
     pattern <- "^[[:alpha:]]*://"
     ftp_dir <- sub(pattern, "", ftp_dir)
-    ## 'RCurl::getURL(ftp_dir)' will fail if 'ftp_dir' does not have a
-    ## trailing slash, so we append the slash but only if it's missing.
+    ## download.file() will fail if 'ftp_dir' does not have a trailing
+    ## slash, so we append the slash but only if it's missing.
     nc <- nchar(ftp_dir)
     last_char <- substr(ftp_dir, nc, nc)
     if (last_char != "/")
         ftp_dir <- paste0(ftp_dir, "/")
     ftp_dir
-}
-
-### Wrapper to RCurl::getURL(). Only purpose of this wrapper is to replace
-### some uninformative error messages returned by RCurl::getURL() with
-### slightly more informative ones.
-.getURL2 <- function(url)
-{
-    stopifnot(isSingleString(url))
-    if (!requireNamespace("RCurl", quietly = TRUE))
-        stop("Install 'RCurl' to connect to Ensembl")
-
-    doc <- try(RCurl::getURL(url), silent=TRUE)
-    if (!inherits(doc, "try-error"))
-        return(doc)
-    condition <- attr(doc, "condition")
-    if (inherits(condition, "REMOTE_ACCESS_DENIED")) {
-        ## Replace uninformative "Server denied you to change
-        ## to the given directory" with more informative message.
-        new_msg <- wmsg("Access to \"", url, "\" denied")
-        condition$message <- new_msg
-    } else if (inherits(condition, "REMOTE_FILE_NOT_FOUND")) {
-        ## Replace uninformative "The file does not exist" with
-        ## more informative message.
-        new_msg <- wmsg("File \"", url, "\" not found on server")
-        condition$message <- new_msg
-    }
-    stop(condition)
 }
 
 .make_matrix_data_from_list <- function(x, min_ncol=0L)
@@ -83,9 +56,8 @@
 ### that is, without the "protolol://" part and with a trailing slash.
 .cached_ftp_dir_listing <- new.env(parent=emptyenv())
 
-### Uses RCurl::getURL() to retrieve the listing of an FTP dir. The result
-### is cached in order to make subsequent calls to list_ftp_dir() on the
-### same 'ftp_dir' faster.
+### Retrieve the listing of an FTP dir. The result is cached in order to make
+### subsequent calls to list_ftp_dir() on the same 'ftp_dir' faster.
 ### If 'long.listing' is TRUE then return a character matrix with 1 row
 ### per entry in the FTP dir listing, and at least 9 columns (but sometimes
 ### more e.g. 11 columns if the listing contains symlinks). The file names
@@ -101,8 +73,7 @@ list_ftp_dir <- function(ftp_dir, subdirs.only=FALSE, long.listing=FALSE,
 
     listing <- .cached_ftp_dir_listing[[ftp_dir]]
     if (is.null(listing) || recache) {
-        doc <- .getURL2(paste0("ftp://", ftp_dir))
-        listing <- strsplit(doc, "\n", fixed=TRUE)[[1L]]
+        listing <- get_ftp_url(paste0("ftp://", ftp_dir))
         .cached_ftp_dir_listing[[ftp_dir]] <- listing
     }
 
