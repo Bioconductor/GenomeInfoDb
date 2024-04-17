5722bb5d0761338a4b3840f858afb73f74787588
Hervé Pagès <hpages.on.github@gmail.com>
2024-04-17 02:17:55 -0700

    Register mCavPor4.1 assembly (domestic guinea pig) from NCBI

---
+++ inst/registered/NCBI_assemblies/Cavia_porcellus.R
@@ -0,0 +1,14 @@
+ORGANISM <- "Cavia porcellus"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    ## 589 sequences.
+    list(assembly="mCavPor4.1",
+         assembly_level="Scaffold",
+         date="2023/12/07",
+         extra_info=c(breed="Dunkin Hartley"),
+         ## GCF_034190915.1 and GCA_034190915.1 are identical.
+         assembly_accession="GCA_034190915.1",
+         circ_seqs="MT")
+)
+
