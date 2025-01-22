6acc7bd383ed92c2907f0bd72930986f977489f7
Hervé Pagès <hpages.on.github@gmail.com>
2025-01-21 20:01:33 -0800

    GenomeInfoDb 1.43.3
    Repair unit test broken (again) by yet another change to NCBI MonDom5
    assembly. This time someone sneakily removed 3900 sequences from the
    assembly, and of course, like last time, the RefSeq accession, GenBank
    accession, and Date were not changed on the assembly page at
    https://www.ncbi.nlm.nih.gov/assembly/237648/
    Looks like the goal is to be as opaque as possible with these changes.
    Interesting approach to data management!

--- inst/registered/NCBI_assemblies/Monodelphis_domestica.R
+++ inst/registered/NCBI_assemblies/Monodelphis_domestica.R
@@ -2,15 +2,27 @@ ORGANISM <- "Monodelphis domestica"
 
 ### List of assemblies by date.
 ASSEMBLIES <- list(
-    ## 9038 sequences. Note that this assembly used to have only 5016
-    ## sequences but, on 2022/06/27, someone added 4022 sequences to it.
-    ## The new sequences are all the sequences after the MT line in
-    ## GCF_000002295.2_MonDom5_assembly_report.txt. Yeah they did this,
-    ## instead of submitting a new version of the assembly like everybody
-    ## else! Also who would have known that NCBI allows submitters to
-    ## silently modify an assembly that they submitted years before without
-    ## any mention on the assembly landing page, without assigning it a new
-    ## RefSeq assembly accession, and without even bumping the assembly date!
+    ## 5138 sequences.
+    ## Note that this assembly used to have only 5016 sequences but, on
+    ## 2022/06/27, someone decided to add 4022 sequences to it, hence
+    ## increasing the number of sequences to 9038. The added sequences are
+    ## all the sequences after the MT line in the assembly report at
+    ## https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/295/GCF_000002295.2_MonDom5/GCF_000002295.2_MonDom5_assembly_report.txt
+    ## Yeah they did this. Instead of submitting a new version of the assembly
+    ## like everybody else! Also who would have known that NCBI allows
+    ## submitters to silently modify an assembly that they submitted years
+    ## before without any mention on the assembly landing page, without
+    ## assigning it a new RefSeq assembly accession, and without even bothering
+    ## to bump the assembly date!
+    ## Wait what? Now on 2025/01/09 they did it again! This time they removed
+    ## 3900 sequences from the assembly, reducing the number of sequences to
+    ## 5138. And again, this modified assembly is still associated with GenBank
+    ## and RefSeq accessions GCA_000002295.1 and GCF_000002295.2, respectively.
+    ## And of course, nobody seemed to think that updating the date of the
+    ## would be a good thing. Date of assembly is still 2007-01-25 on assembly
+    ## report. Great job "The Genome Sequencing Platform, The Genome Assembly
+    ## Team"! Yes, that's the name of the submitter of this assembly. That's
+    ## how whoever submitted this assembly identifies themselves... SMH
     list(assembly="MonDom5",
          date="2007/01/25",
          assembly_accession="GCF_000002295.2",  # monDom5
