719678c16ac9378e68bd518bcfe39223274a8b38
Hervé Pagès <hpages.on.github@gmail.com>
2025-01-23 10:01:38 -0800

    GenomeInfoDb 1.42.3
    Register 3 genome assemblies for Gadus morhua (Atlantic cod):
    
    - GadMor_May2010 (NCBI): https://www.ncbi.nlm.nih.gov/assembly/311358/
    
    - gadMor1 (UCSC, same as GadMor_May2010 but uses UCSC-style chromosome
      names): https://genome.ucsc.edu/cgi-bin/hgGateway?db=gadMor1
    
    - gadMor3.0 (NCBI):
      https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_902167405.1/

---
+++ inst/registered/NCBI_assemblies/Gadus_morhua.R
@@ -0,0 +1,20 @@
+ORGANISM <- "Gadus morhua"
+
+### List of assemblies by date.
+ASSEMBLIES <- list(
+    ## 427427 sequences.
+    list(assembly="GadMor_May2010",
+         assembly_level="Scaffold",
+         date="2011/08/26",
+         assembly_accession="GCA_000231765.1",  # gadMor1
+         circ_seqs=character(0)),
+
+    ## 227 sequences.
+    list(assembly="gadMor3.0",
+         assembly_level="Chromosome",
+         date="2019/07/16",
+         ## GCA_902167405.1 and GCF_902167405.1 are identical.
+         assembly_accession="GCF_902167405.1",
+         circ_seqs="MT")
+)
+
